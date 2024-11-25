1362360f6582fd202e59d85b23b9184cab3df0be
namickey <teethdai@gmail.com>
2024-11-25 15:41:48 +0900

    add

--- excel-filter/1.ps1
+++ excel-filter/1.ps1
@@ -5,8 +5,22 @@ $excelfile = (Get-ChildItem ".\a.xlsx").FullName
 $book = $excel.Workbooks.Open($excelfile)
 $sheet = $book.WorkSheets.Item('a')
 
-#フィルタをかける
-$sheet.Range("E2:F2").AutoFilter(2, "=")
+# E2セルに対してフィルタがかかっているかどうかを判定する
+if ($sheet.AutoFilterMode) {
+    $filterRange = $sheet.AutoFilter.Range
+    $fcell = $filterRange.Address().split(":")[0].replace("$","")
+    #$umu = $filterRange[1].value2
+    
+    if ($fcell -eq "E2") {
+        Write-Output "E2セルにフィルタがかかっています。"
+        $sheet.Range("E2:G2").AutoFilter(2, "=")
+    } else {
+        Write-Output "E2セルにフィルタがかかっていません。"
+        $sheet.Range("F2:G2").AutoFilter(1, "=")
+    }
+} else {
+    Write-Output "シートにフィルタがかかっていません。"
+}
 
 #セルを選択する
 $sheet.Activate()
